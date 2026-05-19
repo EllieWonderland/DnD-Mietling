344b2e753d2b0a45052275a562786be3d21e1cd4
Jana <janaf@Ellie.local>
2026-05-19 13:27:04 +0200

    chore(sw): bump cache to v2 to force SW update and enable media bypass

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'dnd-mietling-v1'
+const CACHE_NAME = 'dnd-mietling-v2'
 const APP_SHELL = ['/', '/index.html', '/manifest.webmanifest', '/pwa-icon.svg']
 
 self.addEventListener('install', event => {
